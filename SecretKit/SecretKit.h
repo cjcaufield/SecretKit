db3767b3728241f03c425e9d8ccc62bed28036c5
Colin Caufield <cjcaufield@gmail.com>
2016-02-15 15:20:31 -0800

    Initial Commit

---
+++ SecretKit/SecretKit.h
@@ -0,0 +1,19 @@
+//
+//  SecretKit.h
+//  SecretKit
+//
+//  Created by Colin Caufield on 2016-02-15.
+//  Copyright © 2016 Secret Geometry, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecretKit.
+FOUNDATION_EXPORT double SecretKitVersionNumber;
+
+//! Project version string for SecretKit.
+FOUNDATION_EXPORT const unsigned char SecretKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecretKit/PublicHeader.h>
+
+
